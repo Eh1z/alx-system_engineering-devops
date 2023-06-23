94f65f1f079fa6e6dd387dcb52719b1e5503a2d3
Godsent Ehiz <111048723+Eh1z@users.noreply.github.com>
2023-06-23 16:31:00 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x17-web_stack_debugging_3/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_3/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,6 @@
-# Web Stack debugging increase limit of open files per user
-exec { 'fix--for-nginx':
-  environment => ['DIR=/etc/default/nginx',
-                  'OLD=ULIMIT="-n 15"',
-                  'NEW=ULIMIT="-n 15000"'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
-  path        => ['/usr/bin', '/bin'],
-  returns     => [0, 1]
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
